1ca6ca553b115d8d1e6c43145bfd4de86920e7ac
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-20 22:17:01 +0530

    added useState hook to form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.scss";
 
 function Form() {
-  return <div className="form">Form</div>;
+  const [file, setFile] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  const allowedType = ["image/jpeg", "image/png", "image/gif"];
+
+  const changeHandler = (event) => {
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile && allowedType.includes(selectedFile.type)) {
+      setFile(selectedFile);
+      setMessage(selectedFile.name);
+    } else {
+      setFile(null);
+      setMessage("Please select valid jpeg, png or gif files");
+    }
+  };
+
+  return (
+    <form className="form">
+      <label>
+        <input type="file" onChange={changeHandler} />
+        <span>+</span>
+      </label>
+      <div className="status">{message}</div>
+    </form>
+  );
 }
 
 export default Form;
