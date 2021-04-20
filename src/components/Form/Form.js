985b0cbaf4632928864a5242957952091460655c
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-20 22:33:14 +0530

    installed framer motion

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import Progress from "../Progress/Progress";
 import "./Form.scss";
 
 function Form() {
   const [file, setFile] = useState(null);
   const [message, setMessage] = useState(null);
+  const [progress, setProgress] = useState("");
 
   const allowedType = ["image/jpeg", "image/png", "image/gif"];
 
@@ -25,7 +27,10 @@ function Form() {
         <input type="file" onChange={changeHandler} />
         <span>+</span>
       </label>
-      <div className="status">{message}</div>
+      <div className="status">
+        {message}
+        {file && <Progress />}
+      </div>
     </form>
   );
 }
